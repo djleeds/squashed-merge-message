a7df57b9b9735fdc050d18bad1c3bec0452601cb
Dave Leeds <david.leeds@daveramsey.com>
2020-04-21 15:37:37 -0500

    Set the ticket number automatically from the pull request title

--- content.js
+++ content.js
@@ -1,9 +1,25 @@
+var findTicketNumber = function() {
+  var pattern = /DEBIT-\d+\b/;
+  var defaultTicketNumber = "DEBIT-XXXX";
+
+  var pullRequestNameField = document.querySelector("#partial-discussion-header .js-issue-title");
+  if(pullRequestNameField) {
+    var pullRequestName = pullRequestNameField.textContent.trim();
+    var match = pullRequestName.match(pattern);
+    if(match) {
+      return match[0];
+    }
+  }
+  
+  return defaultTicketNumber;
+}
+
 var squashMergeMessage = function() {
   var squashButton = document.querySelector('.merge-message .btn-group-squash');
 
   if (!squashButton) return;
 
-  const squashAndMergeMessageTemplate = `https://ramseysolutions.atlassian.net/browse/DEBIT-####
+  const squashAndMergeMessageTemplate = `https://ramseysolutions.atlassian.net/browse/DEBIT-XXXX
 
 Overview: --
 
@@ -17,7 +33,7 @@ Why: --`;
     var messageField = document.getElementById('merge_message_field');
     if (!messageField) return;
 
-    messageField.value = squashAndMergeMessageTemplate;
+    messageField.value = squashAndMergeMessageTemplate.replace("DEBIT-XXXX", findTicketNumber());
   });
 }
 
